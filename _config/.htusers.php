bdfd415f854dc64877dd55063cdfad03dc6e38ba
Sascha Xander <sascha.xander@googlemail.com>
2019-09-12 16:03:55 +0200

    initial commit from xigmaNAS

---
+++ _config/.htusers.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS["users"] = [
+  ['noah','$6$9YkE/29YfAYX9w5g$f3n5ZFuUB40Ik.k3yj7vCbiJ5uSugbcFskreZoSZ9.BrLfxR7fRLS3VsEQOuqu0zt5Hr2MyEVaj4fGqkNvuJX/','/mnt/neo/noah','http://localhost',0,'^.ht',17,1],
+  ['admin','$6$d92QUR6LEmSkcgEJ$83VFo5tICnSW.NJv5i1OCj1bXPpRS84xGlXxVGx1/GV7S/katHEjb/wxDlhxqWhB6ClKFjj3LO7Kw4JM/zPiq1','/','http://localhost',1,'',32847,1],
+  ['root','$6$d92QUR6LEmSkcgEJ$83VFo5tICnSW.NJv5i1OCj1bXPpRS84xGlXxVGx1/GV7S/katHEjb/wxDlhxqWhB6ClKFjj3LO7Kw4JM/zPiq1','/','http://localhost',1,'',32847,1]
+];
+?>
